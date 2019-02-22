c9ab70e5e9e5a90cabd7b1e8cd081c17a3624bd6
DarkTiengo <r_tiengo@hotmail.com>
2019-02-21 22:47:14 -0300

    Update Notes

--- sinteagro/sinteagros/urls.py
+++ sinteagro/sinteagros/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('edit_produtividades/<fazenda_id>/<talhao_id>/',views.edit_produtividades,name='edit_produtividades'),
     path('edit_producao/<fazenda_id>/<talhao_id>/',views.edit_producao,name='edit_producao'),
     path('get_notes/',views.get_notes,name='get_notes'),
+    path('set_note/',views.set_note,name='set_note'),
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
